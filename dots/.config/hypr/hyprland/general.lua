00543d52d36372ae22223176b98a8b9a863804c7
John Heinrich <loldonutph@gmail.com>
2026-09-09 20:34:43 +0800

    hypr: allow tearing

--- dots/.config/hypr/hyprland/general.lua
+++ dots/.config/hypr/hyprland/general.lua
@@ -13,7 +13,7 @@ hl.config({
         border_size = 2,
 
         resize_on_border = false,
-        allow_tearing = false,
+        allow_tearing = true,
         layout = "dwindle",
     },
     decoration = {
