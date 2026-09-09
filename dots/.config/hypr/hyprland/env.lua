029772fd3ed040ad69fbedfecb590440eff6a318
John Heinrich <loldonutph@gmail.com>
2026-09-09 18:17:48 +0800

    hypr(env): add x11 fallback

--- dots/.config/hypr/hyprland/env.lua
+++ dots/.config/hypr/hyprland/env.lua
@@ -5,11 +5,11 @@ hl.env("HYPRCURSOR_SIZE", "24")
 -- Qt
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
-hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 -- Force the use of Wayland
-hl.env("GDK_BACKEND", "wayland,*")
+hl.env("GDK_BACKEND", "wayland,x11,*")
 hl.env("SDL_VIDEODRIVER", "wayland")
 hl.env("CLUTTER_BACKEND", "wayland")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
